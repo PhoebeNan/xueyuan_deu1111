522a47248b88bccb8f6970d351142ef7ccef325f
PhoebeNan <13037650105@163.com>
2020-01-06 17:02:31 +0800

    Initial commit

---
+++ xueyuan_eduservice1111/src/main/java/com/online/edu/eduservice/EduServiceApplication.java
@@ -0,0 +1,16 @@
+package com.online.edu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyanan
+ * @create 2020-01-06-15:27
+ */
+@SpringBootApplication
+public class EduServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduServiceApplication.class,args);
+    }
+}
